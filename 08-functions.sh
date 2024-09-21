556495f79c1768d2e153617aca17117938697fd5
kommagoni Ramesh <kommagoni.ramesh@hcl.com>
2024-09-21 18:55:15 +0530

    10-install-packages.sh

--- 08-functions.sh
+++ 08-functions.sh
@@ -2,28 +2,29 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( echo $0 | cut -d :"." -f1)
-LOGFILE=/temp/SCRIPT_NAME-$TIMESTAMP.log 
+SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo "$2...FAILURE"
-    exit1
-else
-    echo "$2...SUCCESS"
-fi 
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
+
 if [ $USERID -ne 0 ]
 then
-    echo "please run these script withroot access"
-    exit 1
-else 
-    echo "you are super user"
-fi 
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MYSQL"
+VALIDATE $? "Installing MySQL"
 
-dnf install git -y 
-VALIDATE $? "Installing git"
+dnf install git -y &>>$LOGFILE
